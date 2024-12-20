a000532bd0830bb794d768197570025c76441be8
Jonghyo Lee <na1307@outlook.kr>
2024-12-20 23:53:25 +0900

    Fix test

--- BluehillHomePage.Tests/TimeTest.cs
+++ BluehillHomePage.Tests/TimeTest.cs
@@ -5,22 +5,28 @@ using TestContext = Bunit.TestContext;
 namespace BluehillHomePage.Tests;
 
 public sealed class TimeTest : TestContext {
+    private static readonly SeoulTimeProvider stp = new();
+
     [Fact]
     public async Task TimeComponentUpdatesTime() {
         // Arrange
         var cut = RenderComponent<Time>();
 
         // Act
-        var initialDt = DateTime.Now;
+        var initialDt = stp.GetLocalNow();
         var initialTime = cut.FindAll("span.d-flex.justify-content-center")[1].InnerHtml;
         await Task.Delay(2000); // Wait for 2 seconds to allow time to update
         cut.Render(); // Re-render the component
-        var updatedDt = DateTime.Now;
+        var updatedDt = stp.GetLocalNow();
         var updatedTime = cut.FindAll("span.d-flex.justify-content-center")[1].InnerHtml;
 
         // Assert
         Assert.Contains(initialDt.ToString("HH:mm:ss"), initialTime);
         Assert.NotEqual(initialTime, updatedTime);
         Assert.Contains(updatedDt.ToString("HH:mm:ss"), updatedTime);
+    }
+
+    private sealed class SeoulTimeProvider : TimeProvider {
+        public override TimeZoneInfo LocalTimeZone => TimeZoneInfo.FindSystemTimeZoneById("Asia/Seoul");
     }
 }
